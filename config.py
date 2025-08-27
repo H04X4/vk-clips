4f16f4c9d3c5c2bd304ecbbb0bf2a43af6e96abe
UNKNWNCLOWNGHL <58516721+H04X4@users.noreply.github.com>
2025-08-27 23:37:42 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 # VK
-ACCESS_TOKEN = "vk1.a.-"
+ACCESS_TOKEN = "vk1.a.Bp_lHILBk-WCL-----"
 API_VERSION = "5.249"
 PROCESSING_DELAY = 40  # сек, подождать обработку ролика перед edit/publish
 
 # Telegram
 TELEGRAM_TOKEN = "8274120522:"
 ADMIN_CHAT_ID =   # для /start можно ограничить, или оставь None
-
+USE_GROUP = True # False - На страницу, True в сообщество
 # Загрузка
-TOP_COUNT = 2         # сколько брать из топа
+TOP_COUNT = 10         # сколько брать из топа
 VIDEOS_DIR = "videos"    # локальная папка для скачивания
 MAX_LINES_IN_LOG = 1    # сколько строк истории держать в сообщении
+GROUP_ID = #айди сообщества
